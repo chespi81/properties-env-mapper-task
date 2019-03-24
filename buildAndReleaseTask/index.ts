c5acc822ecbc86e16f0bf636094f85c71ec3662a
Roberto San Martin <rsanmartin@tinet.cl>
2019-03-24 02:27:59 -0300

    Se corrige formato de codigo fuente.

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -2,19 +2,19 @@ import tl = require('azure-pipelines-task-lib/task');
 import { PropertiesVariableMapper } from './mapper/properties-variable-mapper';
 
 async function run() {
-    try {
-        const sourceFile: string = tl.getInput('SOURCE_FILE', true);
-        console.log('SOURCE_FILE', sourceFile);
+  try {
+    const sourceFile: string = tl.getInput('SOURCE_FILE', true);
+    console.log('SOURCE_FILE', sourceFile);
 
-        const destFile: string = tl.getInput('DEST_FILE', true);
-        console.log('DEST_FILE', destFile);
+    const destFile: string = tl.getInput('DEST_FILE', true);
+    console.log('DEST_FILE', destFile);
 
-        let mapper: PropertiesVariableMapper = new PropertiesVariableMapper(sourceFile, destFile, tl);
-        mapper.replace();
-    }
-    catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
-    }
+    let mapper: PropertiesVariableMapper = new PropertiesVariableMapper(sourceFile, destFile, tl);
+    mapper.replace();
+  }
+  catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
 }
 
 run();
