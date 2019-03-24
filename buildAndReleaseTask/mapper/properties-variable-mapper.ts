a995c5cb82827e8894d5339ea9323dfedf771f9f
Roberto San Martin <rsanmartin@tinet.cl>
2019-03-24 02:28:13 -0300

    Se mejora manejo de errores en task.

--- buildAndReleaseTask/mapper/properties-variable-mapper.ts
+++ buildAndReleaseTask/mapper/properties-variable-mapper.ts
@@ -14,22 +14,29 @@ export class PropertiesVariableMapper {
 
   public replace(): void {
     readFile(this.source, (err, data: Buffer) => {
-      let input: string = data.toString();
-      let output: string = input;
-      let result = null;
-      while (result = this.expression.exec(input)) {
-        let param: string = result[1];
-        let value: string = this.task.getVariable(param);
-        if (value == null) {
-          value = '';
+      if (err) {
+        console.error('ERROR durante lectura de archivo:', err);
+        this.task.setResult(this.task.TaskResult.Failed, err.message);
+      } else {
+        let input: string = data.toString();
+        let output: string = input;
+        let result = null;
+        while (result = this.expression.exec(input)) {
+          let param: string = result[1];
+          let value: string = this.task.getVariable(param);
+          if (value == null) {
+            this.task.warning('Variable ' + param + ' not found.');
+            value = '';
+          }
+          output = output.replace('${' + param + '}', value);
         }
-        output = output.replace('${' + param + '}', value);
+        writeFile(this.destination, output, err => {
+          if (err) {
+            console.error('ERROR durante creacion de archivo:', err);
+            this.task.setResult(this.task.TaskResult.Failed, err.message);
+          }
+        });
       }
-      writeFile(this.destination, output, error => {
-        if (error) {
-          console.error('ERROR durante creacion de archivo:', error);
-        }
-      });
     });
   }
 }
