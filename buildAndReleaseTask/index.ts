69c5cfc4c479df3bb77cfeb44321e9457da74f4e
Roberto San Martin <rsanmartin@tinet.cl>
2019-03-24 00:36:43 -0300

    Version inicial.

---
+++ buildAndReleaseTask/index.ts
@@ -0,0 +1,20 @@
+import tl = require('azure-pipelines-task-lib/task');
+import { PropertiesVariableMapper } from './mapper/properties-variable-mapper';
+
+async function run() {
+    try {
+        const sourceFile: string = tl.getInput('SOURCE_FILE', true);
+        console.log('SOURCE_FILE', sourceFile);
+
+        const destFile: string = tl.getInput('DEST_FILE', true);
+        console.log('DEST_FILE', destFile);
+
+        let mapper: PropertiesVariableMapper = new PropertiesVariableMapper(sourceFile, destFile, tl);
+        mapper.replace();
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
